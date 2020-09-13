92cc46788b3234647fd2117c718e97d36c2a203d
Yuri Trondin <lopata@2ch.hk>
2020-09-13 10:28:46 +0500

    сделал практическую часть

--- js/script.js
+++ js/script.js
@@ -1,26 +1,55 @@
 "use strict"
 
 window.addEventListener('DOMContentLoaded', () => {
+
+    //переменные
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
-    const tabsPArrent = document.querySelector('.tabheader__items');
+    const tabsParrent = document.querySelector('.tabheader__items');
 
+    //функции
 
+    //скрыть таб
     const hideTabContent = () => {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
     };
 
+
+    //показать таб
     const showTabContent = (i = 0) => {
-        tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
 
     };
 
+
+    //переключаем табы
+    const tabSwitcher = (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    };
+
+
+    //вызов функций
     hideTabContent();
     showTabContent();
+
+
+    //обработчики событий
+    tabsParrent.addEventListener('click', tabSwitcher);
 });
