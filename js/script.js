ad93e72ff9869501d64fbd3dfd596fca38ad46be
Yuri Trondin <lopata@2ch.hk>
2020-09-15 20:24:04 +0500

    сделал таймер

--- js/script.js
+++ js/script.js
@@ -52,4 +52,55 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //обработчики событий
     tabsParrent.addEventListener('click', tabSwitcher);
+
+    const deadline = '2020-10-11';
+
+    const getTimeRemaining = (endTime) => {
+        const time = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(time / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((time / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((time / 1000 / 60) % 60),
+            seconds = Math.floor((time / 1000) % 60);
+
+        return {
+            'total': time,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    };
+
+    const getZero = (num) => {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    };
+    const setClock = (selector, endTime) => {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const time = getTimeRemaining(endTime);
+
+            days.innerHTML = getZero(time.days);
+            hours.innerHTML = getZero(time.hours);
+            minutes.innerHTML = getZero(time.minutes);
+            seconds.innerHTML = getZero(time.seconds);
+
+            if (time.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        };
+    };
+
+    setClock('.timer', deadline);
 });
