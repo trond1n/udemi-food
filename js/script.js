c1d16a19c87df31665f9d1ba7267e774ac448e6f
Yuri Trondin <lopata@2ch.hk>
2020-09-16 20:10:15 +0500

    сделал модальное окно

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -103,4 +103,52 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     setClock('.timer', deadline);
+
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    const openModal = () => {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    };
+
+    const closeModal = () => {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    modalCloseBtn.addEventListener('click', () => {
+        closeModal();
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    const showModalByScroll = () => {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+
+    window.addEventListener('scroll', showModalByScroll);
 });
