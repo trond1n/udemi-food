267be5762fd07eb118cd7e33c85c093c5bb3059e
Yuri Trondin <lopata@2ch.hk>
2020-09-23 18:39:29 +0500

    добавил json server

--- js/script.js
+++ js/script.js
@@ -289,5 +289,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         }, 4000);
     }
-
+    fetch('db.json')
+        .then(data => data.json())
+        .then(res => console.log(res));
 });
