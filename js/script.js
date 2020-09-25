fc3cb56bc65023e398cf9ff372a370ebd4a22069
Yuri Trondin <lopata@2ch.hk>
2020-09-25 18:34:36 +0500

    запилил слайдер

--- js/script.js
+++ js/script.js
@@ -197,9 +197,10 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         return await res.json();
     };
-    getResource('http://localhost:3000/menu')
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({
+            data.data.forEach(({
                 img,
                 altimg,
                 title,
@@ -209,35 +210,6 @@ window.addEventListener('DOMContentLoaded', () => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container', 'menu__item').render();
             });
         });
-
-
-    //тест шаблоном:
-    //         getResource('http://localhost:3000/menu')
-    //         .then(data => createCard(data));
-
-    //     function createCard(data) {
-    //         data.forEach(({
-    //             img,
-    //             altimg,
-    //             title,
-    //             descr,
-    //             price
-    //         }) => {
-    //             const element = document.createElement('div');
-    //             element.classList.add('menu__item');
-    //             element.innerHTML = `
-    // <img src=${img} alt=${altimg}>
-    // <h3 class="menu__item-subtitle">${title}</h3>
-    // <div class="menu__item-descr">${descr}</div>
-    // <div class="menu__item-divider"></div>
-    // <div class="menu__item-price">
-    //     <div class="menu__item-cost">Цена:</div>
-    //     <div class="menu__item-total"><span>${price}</span> грн/день</div>
-    // </div>`;
-    //             document.querySelector('.menu .container').append(element);
-    //         });
-    //     }
-
 
     //Forms
     const forms = document.querySelectorAll('form');
@@ -317,4 +289,46 @@ window.addEventListener('DOMContentLoaded', () => {
     fetch('db.json')
         .then(data => data.json())
         .then(res => console.log(res));
+
+    //слайдер
+
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
 });
